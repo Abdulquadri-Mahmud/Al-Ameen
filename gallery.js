514b9fb760381b4ae88934360b98b1167eb3a305
Abdul-Quadri Mahmud Adeyemi <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2023-02-13 19:11:50 +0100

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,13 @@
+// Gallery Functions
+
+// getting the see more button from html
+const button = document.querySelector('#see_more');
+// Selecting and converting all the images that are hidden
+
+// let hiddenImgaes = Array.from(document.querySelectorAll('.hidden'));
+
+// button.addEventListener('click',(event)=>{
+//      hiddenImgaes.map(hiddenImgae => {
+//           hiddenImgae.style.display = 'block'
+//      })
+// })
